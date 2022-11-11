04ef780669a7d9b3052bfe115bf4aa7526f696aa
utsav Lamichhane <utsav3991@gmail.com>
2022-11-11 18:11:04 +0545

    Business Form

---
+++ apps/forms/busniess_form.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from apps.core.models import Business
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['updated_at', 'created_at']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
