85e3bf5214ecdea9a6d085ec51351468c5059dae
Dipesh Basnet <dipeshbasnet@Dipeshs-MacBook-Pro.local>
2022-11-11 16:58:15 +0545

    [Updated]response

--- apps/core/api/v1/views/business.py
+++ apps/core/api/v1/views/business.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from ..serializer.business import BusinessSerializer
 from ....models import Business
@@ -7,3 +8,14 @@ from ....models import Business
 class BusinessViewSet(viewsets.ModelViewSet):
     queryset = Business.objects.all()
     serializer_class = BusinessSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'data': serializer.data})
